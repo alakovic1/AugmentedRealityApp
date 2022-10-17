9adb585a93d8e7b7c2ebd1520a1a501261a83636
Amila Lakovic <alakovic1@etf.unsa.ba>
2022-10-18 00:05:57 +0200

    AR project

---
+++ Assets/Skripte/alakovic1_infoSkripta.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class alakovic1_infoSkripta : MonoBehaviour {
+
+    public Text infoTekst;
+    private int i = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void onInfo() {
+        if(++i % 2 != 0) infoTekst.text = "";
+        else infoTekst.text = "Dobro došli!" + Environment.NewLine +
+                "Ova aplikacija omogućava predstavljanje" + Environment.NewLine + 
+                "3D modela glavnog obilježja Krakowa u Poljskoj," + Environment.NewLine + 
+                "St. Mary's Basilicu, sagrađenu u 14. stoljeću." + Environment.NewLine + 
+                "Imate mogućnost odabira sporednih objekata," + Environment.NewLine + 
+                "te uz pomoć njih da saznate nešto o ovoj građevini." + Environment.NewLine + 
+                "Lampa pokreće video, krst tekst o cijeni ulaska i" + Environment.NewLine + 
+                "truba (zakačena na toranj) poznati zvuk ove crkve." + Environment.NewLine + 
+                "Prekidate uz pomoć crvenog kruga, a izlazite iz" + Environment.NewLine +
+                "aplikacije na X. Uživajte!!" + Environment.NewLine + 
+                "Amila Laković" + Environment.NewLine + 
+                "Sarajevo, 2020.";
+    }
+}
---
+++ Assets/Skripte/alakovic1_skripta.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class alakovic1_skripta : MonoBehaviour
+{
+
+    Ray ray;
+    RaycastHit hit;
+    public GameObject video;
+    public GameObject stopSfera;
+    public GameObject audioTruba;
+    public GameObject trubackiTekst;
+    public GameObject tekstZaPrekid;
+    public GameObject informacije;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.gameObject.name == "alakovic1_POI2lampa")
+            {
+                video.SetActive(true);
+                stopSfera.SetActive(true);
+                tekstZaPrekid.SetActive(true);
+            }
+            if (hit.collider.gameObject.name == "alakovic1_sferaSTOP")
+            {
+                video.SetActive(false);
+                stopSfera.SetActive(false);
+                audioTruba.SetActive(false);
+                trubackiTekst.SetActive(false);
+                tekstZaPrekid.SetActive(false);
+                informacije.SetActive(false);
+            }
+            if (hit.collider.gameObject.name == "alakovic1_POI3truba")
+            {
+                audioTruba.SetActive(true);
+                stopSfera.SetActive(true);
+                trubackiTekst.SetActive(true);
+                tekstZaPrekid.SetActive(true);
+            }
+            if (hit.collider.gameObject.name == "alakovic1_POI1krst")
+            {
+                informacije.SetActive(true);
+                stopSfera.SetActive(true);
+                tekstZaPrekid.SetActive(true);
+            }
+        }
+    }
+}
